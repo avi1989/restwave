bd3ba7caab5df206a2bf6af3af52559a8bb6db92
Avinash Krishna Kumar <me@avinash.is>
2025-08-07 19:41:34 +0530

    refactor: Split ResponsControl into Modular Files

---
+++ RealRestClient/Views/Response/SimpleResponseControl.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RealRestClient.Views.Response;
+
+public partial class SimpleResponseControl : UserControl
+{
+    public SimpleResponseControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ RealRestClient/Views/Response/StreamingResponseControl.axaml.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using RealRestClient.ViewModels.Responses;
+
+namespace RealRestClient.Views.Response;
+
+public partial class StreamingResponseControl : UserControl
+{
+    private ScrollViewer? _scrollViewer;
+    private ListBox? _listBox;
+    public StreamingResponseControl()
+    {
+        InitializeComponent();
+    }
+    
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+        if (DataContext is ResponseViewModel vm)
+        {
+            vm.StreamLines.CollectionChanged += StreamLines_CollectionChanged;
+        }
+        
+        _listBox = this.FindControl<ListBox>("VirtualizedContent");
+        if (_listBox != null)
+        {
+            _listBox.TemplateApplied += ListBox_TemplateApplied;
+        }
+
+    }
+    private void ListBox_TemplateApplied(object? sender, TemplateAppliedEventArgs e)
+    {
+        _scrollViewer = e.NameScope.Find<ScrollViewer>("PART_ScrollViewer");
+    }
+
+    private void StreamLines_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+        if (_scrollViewer != null)
+        {
+            Dispatcher.UIThread.Post(() => _scrollViewer.ScrollToEnd());
+        }
+    }
+}
